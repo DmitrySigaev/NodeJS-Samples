5131e9bb36fe286379059bf28afa1e5bf2a757b7
DmitrySigaev <dsigaev@yandex.ru>
2015-07-06 21:35:23 +0300

    create module server and add log. results: Debugger listening on port 5858 process.platform: win32 process.version: v0.12.0 show arguments: 0: C:\Program Files\nodejs\node.exe 1: D:\Projects\node\index.js [index.js]: starting [server.js]: starting [server.js]: done. [index.js]: modules initialization [server.js]{export}: Server has started. [index.js]: done.

---
+++ index.js
@@ -0,0 +1,35 @@
+﻿console.log("process.platform: " + process.platform);
+console.log("process.version: " + process.version);
+console.log("show arguments:");
+process.argv.forEach(function (val, index, array) {
+    console.log(index + ': ' + val);
+});
+
+console.logd = function (TYPE, d) {
+    process.stdout.write(__filename + ':' + d + '\n');
+};
+
+/* main code -------------------------------*/
+/* start of file ->*/
+ls = __filename.lastIndexOf('/');
+rs = __filename.lastIndexOf('\\');
+__filename = __filename.substring((((ls >= 0)? ls + 1: 0) | ((rs >= 0)? rs + 1: 0)), __filename.length);
+console.log('[' + __filename + ']' + ": starting");
+exports.done = false;
+/* start of file <- */
+
+/* ----------------------------------------- */
+
+/* declaration of modules  */
+var server = require("./server");
+
+console.log('[' +__filename + ']' + ": modules initialization");
+/* initialization of modules */
+server.start();
+
+/* ----------------------------------------- */
+
+/*end of file ->*/
+exports.done = true;
+console.log('[' + __filename + ']' + ": done.");
+/*end of file <-*/
--- server.js
+++ server.js
@@ -1,12 +1,33 @@
+/* start of file ->*/
+ls = __filename.lastIndexOf('/');
+rs = __filename.lastIndexOf('\\');
+__filename = __filename.substring((((ls>=0)? ls + 1: 0 )| ((rs >= 0)? rs + 1: 0)), __filename.length); 
+console.log('['+__filename +']'+ ": starting");
+exports.done = false;
+/* start of file <- */
+/* ----------------------------------------- */
+
 var http = require("http");
 
-function onRequest(request, response) {
-  console.log("Request received:"+ request.url);
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("Hello World");
-  response.end();
+function start() {
+    function onRequest(request, response) {
+      console.log("Request received:"+ request.url);
+      response.writeHead(200, {"Content-Type": "text/plain"});
+      response.write("Hello World");
+      response.end();
+    }
+
+    http.createServer(onRequest).listen(8888);
+
+    console.log('[' + __filename + ']' + "{export}: Server has started.");
+
 }
 
-http.createServer(onRequest).listen(8888);
+exports.start = start;
 
-console.log("Server has started.");
+/* ----------------------------------------- */
+
+/*end of file ->*/
+exports.done = true;
+console.log('['+__filename + ']' + ": done.");
+/*end of file <-*/
