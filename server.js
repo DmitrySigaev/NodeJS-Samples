d0bead31da2fc48eaea7c033fd01f8eeb2cc9aa2
DmitrySigaev <dsigaev@yandex.ru>
2015-07-06 16:29:37 +0300

    url request shows

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var http = require("http");
 
 function onRequest(request, response) {
-  console.log("Request received.");
+  console.log("Request received:"+ request.url);
   response.writeHead(200, {"Content-Type": "text/plain"});
   response.write("Hello World");
   response.end();
