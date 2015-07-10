95b673ae5aba29051bea43cb5f2405da2385b245
DmitrySigaev <dsigaev@yandex.ru>
2015-07-10 15:24:04 +0300

    this is first try to bind nodejs and indigo project

---
+++ node-indigo.js
@@ -0,0 +1,16 @@
+/* start of file ->*/
+ls = __filename.lastIndexOf('/');
+rs = __filename.lastIndexOf('\\');
+__filename = __filename.substring((((ls>=0)? ls + 1: 0 )| ((rs >= 0)? rs + 1: 0)), __filename.length); 
+console.log('['+__filename +']'+ ": starting");
+exports.done = false;
+/* start of file <- */
+/* ----------------------------------------- */
+
+
+/* ----------------------------------------- */
+
+/*end of file ->*/
+exports.done = true;
+console.log('['+__filename + ']' + ": done.");
+/*end of file <-*/
