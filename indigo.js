dc176e1f66a8aa16d4871ce621cd79445a7c14b6
DmitrySigaev <dsigaev@yandex.ru>
2015-07-14 22:00:30 +0300

    renderer done

--- indigo.js
+++ indigo.js
@@ -15,6 +15,16 @@ IndigoObject = function (d, id, parent) {
     this.id = id;
     this.d = d;
     this.parent = parent;
+
+    this.dispose = function () {
+        if (this.id >= 0)
+            if (this.d._lib != null) {
+                this.d._setSessionId();
+            }
+        this.d._lib.indigoFree(this.id);
+        this.id = -1;
+    }
+
 }
 
 function Indigo() {
@@ -29,7 +39,11 @@ function Indigo() {
         
     var libpath = './indigo-libs/shared/' + process.platform + '/' + process.arch + '/indigo';
     this._lib = ffi.Library(libpath, {
-        "indigoVersion": ["string", []], "indigoAllocSessionId": [qword, []], "indigoSetSessionId": ["void", [qword]]
+        "indigoVersion": ["string", []], 
+        "indigoAllocSessionId": [qword, []],
+        "indigoSetSessionId": ["void", [qword]],
+        "indigoWriteBuffer": ["int", []],
+        "indigoFree":["int", ["int"]]
     });
     
     /* function indigo.vesrion() gets node +indigo versions*/
@@ -42,6 +56,12 @@ function Indigo() {
     this._setSessionId = function () { this._lib.indigoSetSessionId(this._sid) };
     this._checkResult = function (result) { if (result < 0) { throw new Error('indigo:res < 0[' + res + ']') } return result; }
     this._checkResultString = function (result) { console.log(result); return result; }
+    this.writeBuffer = function () {
+        this._setSessionId();
+        id = this._checkResult(this._lib.indigoWriteBuffer());
+        return IndigoObject(this, id);
+    }
+
 }
       
 exports.indigo = Indigo;
--- indigo_renderer.js
+++ indigo_renderer.js
@@ -8,12 +8,17 @@ exports.done = false;
 /* ----------------------------------------- */
 
 var ffi = require('ffi');
+var ref = require('ref');
+var ArrayType = require('ref-array')
+var c_int = ref.types.int;
+var IntArray = ArrayType('int');
+
 
 function IndigoRenderer(indigo) {
     if (!(this instanceof IndigoRenderer)) {
         return new IndigoRenderer(indigo);
     }
-
+    
     if (indigo.done == true) {
         /* module*/
         this.indigo = indigo.indigo();
@@ -31,12 +36,68 @@ function IndigoRenderer(indigo) {
     }
     var libpath = './indigo-libs/shared/' + process.platform + '/' + process.arch + '/indigo-renderer';
     this._lib = ffi.Library(libpath, {
-        "indigoRender": ["int", ["int","int"]],
+        "indigoRender": ["int", ["int", "int"]],
         "indigoRenderToFile": ["int", ["int", "string"]],
-        "indigoRenderGrid": ["int", ["int", "pointer","int", "int"]],
+        "indigoRenderGrid": ["int", ["int", "pointer", "int", "int"]],
         "indigoRenderGridToFile": ["int", ["int", "pointer", "int", "string"]],
         "indigoRenderReset": ["int", ["int"]]
     });
+    
+    this.renderToBuffer = function (obj) {
+        this.indigo._setSessionId();
+        wb = this.indigo.writeBuffer();
+        try {
+            this.indigo._checkResult(this._lib.indigoRender(obj.id, wb.id));
+            return wb.toBuffer();
+        } finally {
+            wb.dispose();
+        }
+    }
+    
+    this.renderToFile = function (obj, filename) {
+        this.indigo._setSessionId();
+        this.indigo._checkResult(this._lib.indigoRenderToFile(obj.id, filename));
+    }
+    
+    this.renderGridToFile = function (objects, refatoms, ncolumns, filename) {
+        this.indigo._setSessionId();
+        arr = null;
+        if (refatoms) {
+            if (refatoms.length != objects.count()) {
+                throw new Error("renderGridToFile(): refatoms[] size must be equal to the number of objects");
+            }
+            arr = new IntArray(refatoms.length);
+            for (i = 0; i < refatoms.length; i++) {
+                arr[i] = refatoms[i];
+            }
+            
+            self.indigo._checkResult(
+                self._lib.indigoRenderGridToFile(objects.id, arr, ncolumns, filename));
+        }
+    }
+    
+    this.renderGridToBuffer = function (objects, refatoms, ncolumns) {
+        this.indigo._setSessionId();
+        arr = null;
+        if (refatoms) {
+            if (refatoms.length != objects.count()) {
+                throw new Error("renderGridToBuffer(): refatoms[] size must be equal to the number of objects");
+            }
+            arr = new IntArray(refatoms.length);
+            for (i = 0; i < refatoms.length; i++) {
+                arr[i] = refatoms[i];
+            }
+            wb = this.indigo.writeBuffer();
+            try {
+                this.indigo._checkResult(
+                    this._lib.indigoRenderGrid(objects.id, arr, ncolumns, wb.id));
+                return wb.toBuffer();
+            } finally {
+                wb.dispose();
+            }
+
+        }
+    }
 }
 
 exports.renderer = IndigoRenderer;
