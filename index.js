5f5ec496909efdbfa20516011f8994944303b35c
DmitrySigaev <dsigaev@yandex.ru>
2015-09-29 20:44:29 +0300

    globalContext: exports === this

--- index.js
+++ index.js
@@ -34,7 +34,35 @@ var a = 10; // variable of the global context
 })();
 
 console.log(a); // 10
-console.log(b); // "b" is not defined
+// check: console.log(b); // "b" is not defined
+/*
+ * Also, many programmers know that the isolated scope in the current version of specification is created 
+ * only by execution contexts with “function” code type. I.e., in contrast with C/C++, for example
+ *  the block of for loop in ECMAScript does not create a local context:
+ *  */
+
+for (var k in { a: 1, b: 2 }) {
+    console.log('k'+ k);
+}
+
+console.log('k' + k); // variable "k" still in scope even the loop is finished
+
+/*
+ * Variable object in global context
+*/
+
+var a = new String('test');
+
+console.log(a); // напрямую, будет найдено в VO(globalContext): "test"
+
+//node.js fix console.log(window['a']); // косвенно через global === VO(globalContext): "test"
+console.log(a === this.a); // true(browser) : false in node.js this.a is undefined
+
+exports.a = a;
+console.log(a === this.a); // true beacause this is exports;
+
+// var aKey = 'a';
+// console.log(window[aKey]); // косвенно, имя свойства сформировано налету: "test"
 
 
 /* ----------------------------------------- */
